60a6a6a243ba305099d510f5943e1a2623e4f9aa
Mike Hall <mike@just3ws.com>
2018-08-11 16:32:54 -0500

    Brute-force list of words from set of letters compared against English dictionary

--- README.md
+++ README.md
@@ -30,6 +30,24 @@ After checking out the repo, run `bin/setup` to install dependencies. Then, run 
 
 To install this gem onto your local machine, run `bundle exec rake install`. To release a new version, update the version number in `version.rb`, and then run `bundle exec rake release`, which will create a git tag for the version, push git commits and tags, and push the `.gem` file to [rubygems.org](https://rubygems.org).
 
+## License
+
+Biscotti is licensed under GPL-3.0-or-later.
+
+This file is part of Biscotti.
+
+Biscotti is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+Biscotti is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+Biscotti. If not, see <https://www.gnu.org/licenses/>.
+
 ## Contributing
 
 Bug reports and pull requests are welcome on GitHub at https://github.com/just3ws/biscotti.
--- spec/biscotti_spec.rb
+++ spec/biscotti_spec.rb
@@ -1,11 +1,79 @@
 # frozen_string_literal: true
 
+require 'pry'
+
+require 'benchmark'
+require 'awesome_print'
+
+module Biscotti
+  module_function
+
+  def letters_frequency_idx(letters)
+    Hash[
+      letters
+      .group_by { |x| letters.count { |y| x == y } }
+      .flat_map do |k, v|
+        v.uniq.map { |l| [l, k] }
+      end
+    ]
+  end
+
+  def library
+    open('/usr/share/dict/words', 'r').readlines # .map { |word| word.strip.downcase.split('') }
+  end
+
+  def word_length_idx(words)
+    words.group_by(&:length)
+  end
+
+  def find_words(input, min_word_length = 2)
+    sources = library
+
+    puts "#{sources.count} words in dictionary"
+
+    letters = input.split('').sort
+
+    letters_frequency_idx = Biscotti.letters_frequency_idx(letters)
+
+    words = letters.count.downto(min_word_length).each_with_object([]) do |letter_count, words|
+      words.concat(letters.permutation(letter_count).to_a.map(&:join))
+    end
+
+    dictionary = sources
+                 .map(&:chomp)
+                 .select { |word| word.length >= min_word_length }
+                 .select { |word| word.length <= letters.count }
+                 .map { |word| word.strip.downcase.split('') }
+                 .select { |word| (word - letters).empty? }
+                 .select { |word| word.all? { |l0| word.count { |l1| l1 == l0 } <= letters_frequency_idx[l0] } }
+                 .map(&:join)
+
+    output = dictionary & words
+  end
+end
+
 RSpec.describe Biscotti do
   it 'has a version number' do
     expect(Biscotti::VERSION).not_to be nil
   end
 
   it 'does something useful' do
-    expect(false).to eq(true)
+    min_word_length = 2
+
+    input = 'fandango'
+
+    output = []
+
+    bmark = Benchmark.realtime do
+      output = described_class.find_words(input)
+    end
+
+    ap [:bmark, bmark]
+
+    puts "#{output.count} words in output"
+    ap described_class.word_length_idx(output)
+
+    # binding.pry
+    # puts
   end
 end
