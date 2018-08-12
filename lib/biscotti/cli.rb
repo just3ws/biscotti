41940819a189dc442909852d557f3a5a97a553c4
Mike Hall <mike@just3ws.com>
2018-08-11 21:00:03 -0500

    Simple commandline interface

---
+++ lib/biscotti/cli.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Biscotti
+  module CLI
+    def self.run(app, io)
+      # app.new(railroad: Sodor::Railroad::Marshal.load(io))
+    end
+
+    def self.processable?
+      ARGF.filename != '-' || !(STDIN.tty? || STDIN.closed?)
+    end
+  end
+end
